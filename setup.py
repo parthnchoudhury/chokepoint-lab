27186589f1a51770af1cf7a5de62f43407c396ee
Partho CHOUDHURY <76818972+parthnchoudhury@users.noreply.github.com>
2025-06-26 16:17:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,31 +3,31 @@ from Cython.Build import cythonize
 from setuptools import setup, find_packages, Extension
 
 extensions = [
-    Extension('chokepoint-lab.simulations.sim_summary',
-              ['chokepoint-lab/simulations/sim_summary.pyx']),
-    Extension('chokepoint-lab.simulations.monte_carlo_simulation',
-              ['chokepoint-lab/simulations/monte_carlo_simulation.pyx']),
-    Extension('chokepoint-lab.inventory.eoq', ['chokepoint-lab/inventory/eoq.pyx']),
-    Extension('chokepoint-lab.demand._squared_error',
-              ['chokepoint-lab/demand/_squared_error.pyx']),
-    Extension('chokepoint-lab.demand._evo_algo',
-              ['chokepoint-lab/demand/_evo_algo.pyx']),
+    Extension('supplychainpy.simulations.sim_summary',
+              ['supplychainpy/simulations/sim_summary.pyx']),
+    Extension('supplychainpy.simulations.monte_carlo_simulation',
+              ['supplychainpy/simulations/monte_carlo_simulation.pyx']),
+    Extension('supplychainpy.inventory.eoq', ['supplychainpy/inventory/eoq.pyx']),
+    Extension('supplychainpy.demand._squared_error',
+              ['supplychainpy/demand/_squared_error.pyx']),
+    Extension('supplychainpy.demand._evo_algo',
+              ['supplychainpy/demand/_evo_algo.pyx']),
 ]
 # only required for pypi
 here = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(here,'LOG.txt')) as f:
     long_description = f.read().strip()
 
-setup(name='chokepoint-lab',
+setup(name='supplychainpy',
       version='0.0.5',
       description='A library for supply chain, operations and manufacturing, analysis, modelling and simulation.',
-      url='https://github.com/KevinFasusi/chokepoint-lab',
-      download_url='https://github.com/KevinFasusi/chokepoint-lab/tarball/0.0.5',
+      url='https://github.com/KevinFasusi/supplychainpy',
+      download_url='https://github.com/KevinFasusi/supplychainpy/tarball/0.0.5',
       author='Kevin Fasusi',
       author_email='kevin@supplybi.com',
       license='BSD 3',
       packages=find_packages(exclude=['docs', 'tests', 'scratch.py']),
-      test_suite='chokepoint-lab/tests',
+      test_suite='supplychainpy/tests',
       install_requires=['numpy',
                         'cython',
                         'flask',
@@ -48,7 +48,7 @@ setup(name='chokepoint-lab',
       ext_modules=cythonize(extensions),
       entry_points={
           'console_scripts': [
-              'supplychainpy = chokepoint-lab.supplychain:main'
+              'supplychainpy = supplychainpy.supplychain:main'
           ]
       },
       long_description= long_description,
@@ -58,5 +58,5 @@ setup(name='chokepoint-lab',
         "Programming Language :: Python :: 3.5",
     ],
       package_data={
-          'chokepoint-lab': ['reporting/dashboard/templates/dashboard/*','reporting/*','reporting/static/images/*','reporting/static/fonts/*','reporting/static/scripts/*','reporting/static/styles/*','reporting/static/*', 'reporting/templates/*', 'sample_data/*.csv', 'sample_data/*.py','_pickled/*']
+          'supplychainpy': ['reporting/dashboard/templates/dashboard/*','reporting/*','reporting/static/images/*','reporting/static/fonts/*','reporting/static/scripts/*','reporting/static/styles/*','reporting/static/*', 'reporting/templates/*', 'sample_data/*.csv', 'sample_data/*.py','_pickled/*']
       })
